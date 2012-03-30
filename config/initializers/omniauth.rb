ebc72a27c5a775903a90995e17979c2dc594b2bc
Konstantinos Petridis <a6973416317@gmail.com>
2012-03-30 23:53:05 +0300

    test

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -11,7 +11,7 @@ require 'openid/store/filesystem'
   provider :facebook, '338155269553812', '580211ee842609e26b73b9a7ad6df291'
 
   elsif Rails.env.production?
-     provider :twitter, '', ''
+     provider :twitter, 'OFwqZ9VRfYZT0BCO8f1Xxg', 'XaLr9oWtI5V6b515vmM9UM6S8vQVpROTsk8DQGHcdTA'
      provider :facebook, '280314415372452', 'c52f299262ca93acb4871685b8e7771e'
   end  
 end
